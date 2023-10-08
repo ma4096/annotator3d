46455a0a94014795015babd951cfb2c7b160b526
Mathis <55271872+ma4096@users.noreply.github.com>
2023-10-08 22:54:52 +0200

    Restructuring
    This project will be merged with explorer3d, as it should be ideally run in the same directory.

---
+++ scriptf.js
@@ -0,0 +1,20 @@
+// Only cosmetics for the index.html and style.css
+// No interactions with main content or server
+
+var navIsOpen = true;
+function navOpenClose() {
+	var sidebar = document.getElementsByTagName("sidebar")[0];
+	var navButton = document.getElementById("navButton");
+	sidebar.style.width = navIsOpen ? "0%" : "20%";
+	navButton.style.marginLeft = navIsOpen ? "0%" : "20%";
+	navButton.innerText = (navIsOpen ? "Open Menu" : "Close Menu");
+	navIsOpen = !navIsOpen;
+}
+
+var anoIsOpen = false;
+function anoOpenClose() {
+	document.getElementsByClassName("annotation-selector")[0].style.height = anoIsOpen ? "0%" : "20%";
+	document.getElementById("anoButton").style.bottom = anoIsOpen ? "0%" : "20%";
+	document.getElementById("anoButton").innerText = (anoIsOpen ? "Open Animations" : "Close Animations");
+	anoIsOpen = !anoIsOpen;
+}
